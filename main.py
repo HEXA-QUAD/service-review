fb17111fc8ff40378435c9faf7f934c57c07e058
NingHsia <107270138+NingHsia@users.noreply.github.com>
2023-10-19 22:56:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Response
+import uvicorn
+
+app = FastAPI()
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8012)
